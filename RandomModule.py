6cb76a6af5a52807a6f62fc78841e39706846963
username <davidkinyanjui052@gmail.com>
2021-04-04 10:54:17 +0300

    Changed the study material

---
+++ RandomModule.py
@@ -0,0 +1,53 @@
+#PRNG-->Pseudorandom Number Generator(The random numbers generated using Python,,Done by software)
+#TRNG-->True Random Number Generator eg. Tossing a die in the air(Done by hardware)
+class NotSoRandom(object):
+    def seed(self,a=3):
+        self.seedval=a
+    def random(self):
+        self.seedval=(self.seedval*3%19)
+        return self.seedval
+_inst=NotSoRandom()
+seed=_inst.seed
+random=_inst.random
+seed(1234)
+print([random() for _ in range(10)])
+#Trial in my own way
+import random
+print(random.random())
+l=[]
+for i in range(10):
+    l.append(i)
+print(l)
+#Deterministic Random
+# random.seed(444)
+print(random.random())
+print(random.random())
+#Randint
+r=random.randint(0,10)
+print(r)
+r1=random.randint(500,5000)
+print(r1)
+#Random RandRange
+print(random.randrange(1,10))
+print(random.randrange(100,500))
+#Random.uniform
+print(random.uniform(20,30))
+print(random.uniform(30,40))
+#Random.choice, Choice is used with the collective data types
+items=["one",'two','three',"four",'five']
+print(random.choice(items))
+game=["1","X","2"]
+print(random.choice(game))
+class Add:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def __add__(self,other):
+        return self.a + self.other
+if __name__=="__main__":
+    a=Add(5,6)
+    print(a.a+a.b)
+#Changed the study material
+import random
+print(random.randint(0,5))
+
