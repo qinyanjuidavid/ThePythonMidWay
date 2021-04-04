ddd4c863ac7a7cc306c9bec7b80ca21caa05f28a
username <davidkinyanjui052@gmail.com>
2021-04-04 11:15:05 +0300

    Worked on the choice() where i found it usefull in the deque function

--- RandomModule.py
+++ RandomModule.py
@@ -54,4 +54,32 @@ print(random.random())#It is giving us a float
 print(random.random()*100)#We can multiply it, so that we get the output in the range that we want
 print(random.random()*1000)#Now it ranges between 0,1000
 print(random.randint(0,100))#Same as the above line
-
+#Choice
+#Choice is used with the collective data types
+l=["Red","Black","Green"]#Using a list
+print(random.choice(l))
+t=(3.1,5.6,7.9,32.1)#Using a tuple
+print(random.choice(t))
+# s={"Orange","Banana","Banana","Water Melon","Strawberry"}#Using a set
+# print(random.choice(s)) Set can't be used in this case
+d={
+    "Jamie":32000,
+    "Angie":45000,
+    "Jane Doe":28000,
+    "John Doe":56000
+}
+# print(random.choice(d)) Dictionaries cant be used with choice
+print(d.keys())
+d=list(d.keys())
+print(random.choice(d))#Thus we have to device a way by converting the keys into a list
+from collections import deque
+#Lets try using the deque which is almost as similar as a list
+name="J"
+a=deque(name)
+a.append("John Doe")
+a.popleft()
+a.append("Jane Doe")
+a.append("Jamie Saint Patric")
+a.append("Angie Valdes")
+print(a)
+print(random.choice(a))
