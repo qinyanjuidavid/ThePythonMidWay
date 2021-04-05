f5395827e21d9fbe27ef8328d480fb9b2854b08b
username <davidkinyanjui052@gmail.com>
2021-04-05 22:10:15 +0300

    Finished learning the basics of decorators

--- HandsOnDecorators.py
+++ HandsOnDecorators.py
@@ -46,5 +46,37 @@ display()
 def display_info(name,age):
     print("display_info ran with arguments {}, {}".format(name,age))
 display_info("John Doe",25)
+print()
+#Practical examples
+from functools import wraps
+def my_logger(original_function):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(original_function.__name__),level=logging.INFO)
+    @wraps(original_function)
+    def wrapper(*args,**kwargs):
+        logging.info(
+            "Ran with arguments: {}, and kwargs: {}".format(args,kwargs)
+        )
+        return original_function(*args,**kwargs)
+    return wrapper
 
+def my_timer(original_function):
+    import time
+    @wraps(original_function)
+    def wrapper(*args,**kwargs):
+        t1=time.time()
+        result=original_function(*args,**kwargs)
+        t2=time.time()
+        t3=t2=t1
+        print('{} ran in: {} sec'.format(original_function.__name__,t3))
+        return result
+    return wrapper
+@my_logger
+@my_timer
+def display(name,age):
+    print('display function ran with arguments {} and {}'.format(name,age))
+display("John Doe",25)
+display("Jane Doe",24)
+display("Angie",28)
+display("Jamie",30)
 
