a51e4de4a4a9ad1fa64ccbc0b8a37af7a89b7c49
username <davidkinyanjui052@gmail.com>
2021-04-05 16:13:09 +0300

    Delt with writing in files using the with,

--- Reading&WritingInFiles.py
+++ Reading&WritingInFiles.py
@@ -30,3 +30,19 @@ for i in range(number_of_students):
     Total: {}
     Avg Marks: {}""".format(i+1,students_name,english,kiswahili,maths,science,sst,total,average))
     m.close()
+#Reading Binary Files
+f=open("coderpass.png","r+b")
+print(f)
+f.close()
+try:
+    f=open('encoded.txt','w',encoding="utf-8")
+    f.write("Hello Friend,")
+finally:
+    f.close()
+with open("test.txt",'w',encoding='utf-8') as f:
+    f.write("Hello friend....")
+with open('test.txt','w',encoding='utf-8') as f:
+    f.write("This is my first line\n")
+    f.write("This line\n")
+    f.write("Contains only three lines.")
+
