bbd0351cf2893a4ea8a985218b91319d5185a672
username <davidkinyanjui052@gmail.com>
2021-04-05 21:38:48 +0300

    Delted with the class decorators...

--- HandsOnDecorators.py
+++ HandsOnDecorators.py
@@ -12,13 +12,26 @@ bye_func()
 def decorator_function(f):
     def wrapper_function(*args,**kwargs):
         print("Function: {}".format(f.__name__))
-        return f()
+        return f(*args,**kwargs)
     return wrapper_function
 
 @decorator_function
 def Trial():
     print("Hello")
 Trial()
+#Class Decorators
+class DecoratorClass(object):
+    def __init__(self,original_function):
+        self.original_function=original_function
+    def __call__(self,*args,**kwargs):
+        print("Function: {}".format(self.original_function.__name__))
+        return self.original_function(*args,**kwargs)
+@DecoratorClass
+def Display():
+    print("It worked..")
+Display()
+
+
 
 
 
