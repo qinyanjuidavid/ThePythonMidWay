87234b459a8a2303c8b898beffee8220b5832f48
username <davidkinyanjui052@gmail.com>
2021-04-05 10:40:19 +0300

    Working on the lambda functions.

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -6,3 +6,14 @@ print(i)
 print((lambda x:x+1)(2))
 print((lambda name:name) ("John"))
 #Lambda is an expression thus it can be named
+add_one=lambda x :x +1
+print(add_one(2))
+print(add_one(5))
+#eg
+identity=lambda name:"My name is {}.".format(name)
+print(identity("John Doe"))
+print(identity("Jane Doe"))
+#eg2
+identity=lambda name,age,department:"My name is {}, I am {} years old. I am in the {} department".format(name,age,department)
+i1=identity("Angie Valdosa",28,"Human Resource")
+print(i1)
