f095e1a674df2fff41f91777b6bceb2c119079f8
username <davidkinyanjui052@gmail.com>
2021-04-06 19:19:50 +0300

    Hands on a bit of the first class functions

--- derocators.py
+++ derocators.py
@@ -4,3 +4,15 @@ def add_one(number):
 print(add_one(5))
 print(add_one(88))
 #First-class objects
+def say_hello(name):
+    return f"Hello {name}"
+print(say_hello("John Doe"))
+
+def be_awesome(name):
+    return f"Yo {name},together we are the awesomest!"
+print(be_awesome("Jane Doe"))
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
+print(greet_bob(say_hello))
+print(greet_bob(be_awesome))
