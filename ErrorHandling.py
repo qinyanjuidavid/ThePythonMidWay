8a344cbacbc7e44d8f0733be2fede0fd7a57388b
username <davidkinyanjui052@gmail.com>
2021-04-07 22:02:06 +0300

    Raised an exception using the if statement, This helps in indicating where the cause of the error is...

---
+++ ErrorHandling.py
@@ -0,0 +1,7 @@
+#Raising an exception
+x=5
+if x>5:
+    raise Exception("X should not exceed 5. The value of x was:{}".format(x))
+#An exception is raised indicating where the error is
+print("Hello friend")
+
