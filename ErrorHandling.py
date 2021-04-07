4d5cbcef72857d646253ef6012273929e5ba26f1
username <davidkinyanjui052@gmail.com>
2021-04-07 22:14:04 +0300

    Error handling using the try and except blocks

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -5,3 +5,28 @@ if x>5:
 #An exception is raised indicating where the error is
 print("Hello friend")
 
+#AssertionError Exception
+import sys
+def linux_interaction():
+    assert ('linux' in sys.platform)
+    print("Doing something")
+linux_interaction()
+#Try and Except block
+import sys
+def linux_interaction():
+    assert ("linux" in sys.platform)
+    print("Doing Something")
+try:
+    linux_interaction()
+except AssertionError as error:
+    print(error)
+    print("The linux function was not excuted")
+#The above function only runs on linux
+def Divide(n,l):
+    a=n/l
+    print(a)
+Divide(6,3)
+try:
+    Divide(0,0)
+except ZeroDivisionError as error:
+    print(error)
