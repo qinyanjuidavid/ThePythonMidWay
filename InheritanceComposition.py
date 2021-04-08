47e3c946c755e46c582d147783b9e560b89a7909
username <davidkinyanjui052@gmail.com>
2021-04-08 20:20:27 +0300

    Worked on the hourly employee

--- InheritanceComposition.py
+++ InheritanceComposition.py
@@ -18,4 +18,11 @@ class SalaryEmployee(Employee):
         self.weekly_salary=weekly_salary
     def calculate_payroll(self):
         return self.weekly_salary
-    
+class HourlyEmployee(Employee):
+    def __init__(self,id,name,hours_worked,hour_rate):
+        super().__init__(id,name)
+        self.hours_worked=hours_worked
+        self.hour_rate=hour_rate
+    def calculate_payroll(self):
+        return self.hours_worked*self.hour_rate
+
