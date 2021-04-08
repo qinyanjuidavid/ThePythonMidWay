13841503b7829d711309983987ae2b6981c531b3
username <davidkinyanjui052@gmail.com>
2021-04-08 20:05:10 +0300

    Delt with the basics of FunctionComposition

--- FunctionComposition.py
+++ FunctionComposition.py
@@ -13,3 +13,14 @@ add(5)
 def multiplication(x):
     return x*2
 print(multiplication(add(5)))
+#Better way to implement composition
+def composition_function(f,g):
+    return lambda x:f(g(x))
+
+def add(x):
+    return x+2
+
+def multiplication(x):
+    return x*2
+add_multiply=(composition_function(multiply,add))
+print(add_multiply(6))
